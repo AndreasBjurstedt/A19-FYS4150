fbdf5286895e0eb3938001b84b17b477d8679bd6
Andreas Bjurstedt <andreas.bjurstedt@hotmail.no>
2019-08-23 15:28:01 +0200

    Create testfile.py

---
+++ testfile.py
@@ -0,0 +1,17 @@
+#A little testfile
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-2,2.2,0.2)
+y = x**2
+z = x**3
+
+fig = plt.figure
+ax1 = plt.subplot(211)
+ax1.plot(x,y)
+
+ax2 = plt.subplot(212)
+ax2.plot(x,z)
+plt.show()
+
+print(x)
